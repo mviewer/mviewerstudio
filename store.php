2d8b2be9ac91a83930f875ab7a5cc57d2b72efb9
spelhate <seb.pelhate@gmail.com>
2017-12-20 14:03:51 +0100

    New code organisation

--- store.php
+++
@@ -1,9 +0,0 @@
-<?php
-$_conf = json_decode(file_get_contents("config.json"), true)["app_conf"];
-$xml = file_get_contents('php://input');
-$fichier = hash('md5', $xml) . '.xml';
-file_put_contents($_conf['generator_folder'] .$fichier, $xml);
-header('Content-type: application/json',true);   
-echo '{"success":true, "url":"'. $_conf['mviewer_instance'] .'?config='. $_conf['generator_path_from_mviewer'] .$fichier.'"}'; 
-  
-?>
