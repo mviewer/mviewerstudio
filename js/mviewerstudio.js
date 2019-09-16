b2093427a13e13dd7d7d928289722e4d3b5b83cd
spelhate <seb.pelhate@gmail.com>
2019-09-16 14:16:09 +0200

    Fix #28

--- js/mviewerstudio.js
+++ js/mviewerstudio.js
@@ -159,6 +159,8 @@ $(document).ready(function(){
 //EPSG:2154
 proj4.defs("EPSG:2154","+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
 
+ol.proj.proj4.register(proj4);
+
 //Map
 var map = new ol.Map({
     layers: [
