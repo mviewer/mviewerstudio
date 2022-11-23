2c8bcb6fd9da042bc66380f4e41cb5978c747d2b
Agath21 <agathe.adam@jdev.fr>
2022-11-23 12:27:49 +0100

    Design of options for app module #134

--- js/manageDisplayContainer.js
+++ js/manageDisplayContainer.js
@@ -10,3 +10,25 @@ function showHome() {
     document.getElementById("containerStudio").hidden = true;
     document.getElementById("containerHome").hidden = false;
 }
+
+// Manage display of options
+
+const activeSearch = document.getElementById('SwitchAdressSearch');
+
+activeSearch.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    document.getElementById('AppSearchBlock').style.display = "block"; 
+  } else {
+    document.getElementById('AppSearchBlock').style.display = "none";
+  }    
+})
+
+const activeElasSearch = document.getElementById('SwitchElastic');
+
+activeElasSearch.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    document.getElementById('FadvElasticBlock').style.display = "block"; 
+  } else {
+    document.getElementById('FadvElasticBlock').style.display = "none";
+  }    
+})
