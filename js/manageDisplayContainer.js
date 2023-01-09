504a291e7048e595a0c60858f509915eda9f2cdf
lecault <loic.ecault@bretagne.bzh>
2023-01-09 10:56:15 +0100

    modif parametre recherche adresse

--- js/manageDisplayContainer.js
+++ js/manageDisplayContainer.js
@@ -43,7 +43,7 @@ $( "#navWizApp" ).click(function() {
 // Manage display of options
 const activeSearch = document.getElementById('SwitchAdressSearch');
 activeSearch.addEventListener('change', (event) => {
-  const isDisplay = event.currentTarget.checked ? "block" : "none";
+  const isDisplay = event.currentTarget.checked ? "block" : "ban";
   document.getElementById('AppSearchBlock').style.display = isDisplay;      
 })
 
