fb15323843c21de87eca938cfe012e814c1765a6
gaetanbrl <gaetan.bruel@jdev.fr>
2023-02-23 19:02:54 +0100

    rename route

--- srv/python/mviewerstudio_backend/route.py
+++ srv/python/mviewerstudio_backend/route.py
@@ -32,11 +32,11 @@ def basic_store_init(state: BlueprintSetupState):
 def default_doc():
     return redirect("index.html")
 
-@basic_store.route("/srv/user_info", methods=["GET"])
+@basic_store.route("/api/user", methods=["GET"])
 def user() -> Response:
     return jsonify(current_user.as_dict())
 
-@basic_store.route("/srv/config", methods=["POST"])
+@basic_store.route("/api/app", methods=["POST"])
 def store_mviewer_config() -> Response:
     config = Config(
         request.data,
@@ -54,7 +54,7 @@ def store_mviewer_config() -> Response:
     return jsonify({"success": True, "filepath": config_data.url, "config": config_data})
 
 
-@basic_store.route("/srv/list", methods=["GET"])
+@basic_store.route("/api/app", methods=["GET"])
 def list_stored_mviewer_config() -> Response:
     """
     Return all mviewer config created by the current user
@@ -65,39 +65,35 @@ def list_stored_mviewer_config() -> Response:
         config["url"] = current_app.config["CONF_PATH_FROM_MVIEWER"] + config["url"]
     return jsonify(configs)
 
+def delete_workspace(id):
+    register = current_app.register
+    config = register.read(id)
+    if config :
+        # update json
+        register.delete(config[0])
+        # delete directory
+        workspace = path.join(current_app.config["EXPORT_CONF_FOLDER"], id)
+        rmtree(workspace)
 
-@basic_store.route("/srv/list", methods=["DELETE"])
-def delete_configs_workspace() -> Response:
-    
-    data = request.get_json()
-    
-    if not data["ids"]:
-        raise BadRequest("Nothing to delete !")
-    
-    for id in data:
-        delete_config_workspace(id)
-    
-    return jsonify({"deleted_files": len(data["ids"]), "success": True, "message": "Configs removed !"})
-
-@basic_store.route("/srv/config/<id>", methods=["DELETE"])
-def delete_config_workspace(id) -> Response:
+@basic_store.route("/api/app", methods=["DELETE"])
+def delete_config_workspace() -> Response:
     """
     Delete one mviewer config
     """
-    register = current_app.register
-    
-    configs = register.read(id)
-    if not configs :
-        raise BadRequest("Version's ID does not exists !")
+    app_deleted = 0
 
-    # update json
-    register.delete(configs[0])
-    # delete directory
-    workspace = path.join(current_app.config["EXPORT_CONF_FOLDER"], id)
-    rmtree(workspace)
-    return jsonify({"deleted_files": 1, "success": True})
+    post_data = request.get_json()
+
+    if not post_data["ids"]:
+        raise BadRequest("Empty list : no value to delete !")
+
+    for id in post_data["ids"]:
+        delete_workspace(id)
+        app_deleted += 1
+
+    return jsonify({"deleted_files": app_deleted, "success": True})
   
-@basic_store.route("/srv/clean", methods=["DELETE"])
+@basic_store.route("/api/app/all", methods=["DELETE"])
 def delete_all_mviewer_config() -> Response:
     """
     Delete all the mviewer config of the user logged
@@ -110,8 +106,64 @@ def delete_all_mviewer_config() -> Response:
         count += 1
     return jsonify({"deleted_files": count, "success": True, "message": "Workspace is clean and empty !"})
 
+@basic_store.route("/api/app/<id>/version/<version>", methods=["PUT"])
+def switch_app_version(id, version = "1") -> Response:
+    '''
+    Allow to switch version
+    '''
+    # read GET params from URL
+    as_new = request.args.get('as_new', default=False, type=bool)
+    config =  current_app.register.read(id)
 
-@basic_store.route("/srv/store/style", methods=["POST"])
+    if not version or version == "1":
+        as_new = True
+
+    if not config:
+        raise BadRequest("This config doesn't exists !")
+    
+    workspace = path.join(current_app.config["EXPORT_CONF_FOLDER"], config[0].id)
+    git = Git_manager(workspace)
+    git.switch_version(version, as_new)
+    return jsonify({"success": True, "message": "Version changes !"}), 200       
+
+@basic_store.route("/api/app/<id>/version", methods=["DELETE"])
+def delete_app_versions() -> Response:
+    '''
+    Delete each app versions
+    Only keep main active branch
+    '''
+    version_deleted = 0
+
+    post_data = request.get_json()
+
+    if not post_data["versions"]:
+        raise BadRequest("Empty list - Nothing to delete !")
+
+    config =  current_app.register.read(id)
+    if not config :
+        raise BadRequest("This config doesn't exists !")
+
+    workspace = path.join(current_app.config["EXPORT_CONF_FOLDER"], config[0].id)
+    git = Git_manager(workspace)
+
+    for version in post_data["version"]:
+        git.delete_version(version)
+        version_deleted += 1
+
+    return jsonify({"success": True, "deleted": version_deleted})
+
+@basic_store.route("/api/app/<id>/version", methods=["POST"])
+def create_app_version(id) -> Response:
+    config =  current_app.register.read(id)
+    if not config:
+        raise BadRequest("This config doesn't exists !")
+    
+    workspace = path.join(current_app.config["EXPORT_CONF_FOLDER"], config[0].id)
+    git = Git_manager(workspace)
+    git.create_version()
+    return jsonify({"success": True, "message": "New version created !"}), 200      
+
+@basic_store.route("/api/app/style", methods=["POST"])
 def store_style() -> Response:
     """
     This endpoint stores SLD style locally. it does not verify the content.
@@ -120,7 +172,7 @@ def store_style() -> Response:
     filehash = hashlib.sha256()
     filehash.update(raw_style.encode("utf-8"))
     filename = f"{filehash.hexdigest()}.sld"
-    absolute_path = os.path.join(
+    absolute_path = path.join(
         current_app.config["EXPORT_CONF_FOLDER"], "styles", filename
     )
     with open(absolute_path, "w") as f:
@@ -128,12 +180,11 @@ def store_style() -> Response:
     return jsonify(
         {
             "success": True,
-            "filepath": os.path.join(
+            "filepath": path.join(
                 current_app.config["CONF_PATH_FROM_MVIEWER"], "styles", filename
             ),
         }
     )
-
 
 @basic_store.route("/proxy/", methods=["GET", "POST"])
 def proxy() -> Response:
@@ -157,60 +208,3 @@ def proxy() -> Response:
     else:
         raise BadRequest("Missing param : url")
     return response
-
-@basic_store.route("/srv/version/<id>/<version>", methods=["GET"])
-def change_config_version(id, version = "1") -> Response:
-    '''
-    Allow to change version to test another
-    '''
-    # read GET params from URL
-    as_new = request.args.get('as_new', default=False, type=bool)
-    config =  current_app.register.read(id)
-    if not version or version == "1":
-        as_new = True
-    if config:
-        workspace = path.join(current_app.config["EXPORT_CONF_FOLDER"], config[0].id)
-        git = Git_manager(workspace)
-        git.switch_version(version, as_new)
-        return jsonify({"success": True, "message": "Version changes !"}), 200
-    else :
-        raise BadRequest("This config doesn't exists !")
-
-@basic_store.route("/srv/version/<id>/<version>", methods=["DELETE"])
-def delete_config_version(id, version) -> Response:
-    '''
-    Allow to change version to test another
-    '''
-    config =  current_app.register.read(id)
-    if config:
-        workspace = path.join(current_app.config["EXPORT_CONF_FOLDER"], config[0].id)
-        git = Git_manager(workspace)
-        git.delete_version(version)
-        return jsonify({"success": True, "message": "Version changes !"}), 200
-    else :
-        raise BadRequest("This config doesn't exists !")
-
-@basic_store.route("/srv/version/<id>", methods=["DELETE"])
-def delete_all_config_version(id) -> Response:
-    '''
-    Allow to change version to test another
-    '''
-    config =  current_app.register.read(id)
-    if config:
-        workspace = path.join(current_app.config["EXPORT_CONF_FOLDER"], config[0].id)
-        git = Git_manager(workspace)
-        git.delete_versions()
-        return jsonify({"success": True, "message": "Version changes !"}), 200
-    else :
-        raise BadRequest("This config doesn't exists !")
-    
-@basic_store.route("/srv/version/<id>", methods=["POST"])
-def create_config_version(id) -> Response:
-    config =  current_app.register.read(id)
-    if config:
-        workspace = path.join(current_app.config["EXPORT_CONF_FOLDER"], config[0].id)
-        git = Git_manager(workspace)
-        git.create_version()
-        return jsonify({"success": True, "message": "New version created !"}), 200
-    else :
-        raise BadRequest("This config doesn't exists !")
