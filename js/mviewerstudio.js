43cc862a1272adeb28bb5b14f374a009e5b35ab7
gaetanbrl <gaetan.bruel@jdev.fr>
2023-03-07 12:15:00 +0100

    add comment param value for save

--- js/mviewerstudio.js
+++ js/mviewerstudio.js
@@ -727,7 +727,10 @@ let previewAppsWithoutSave = (id) => {
         .catch(err => alert(mviewer.tr('msg.xml_doc_invalid')))
 }
 
-var saveApplicationParameters = function (option) {
+var saveApplicationParameters
+    // option == 0 : save serverside
+    // option == 1 : save serverside + download
+    // option == 2 : save serverside + launch map    = function (option) {
     const conf = getConfig();
     if (mv.validateXML(conf.join(""))) {
 
