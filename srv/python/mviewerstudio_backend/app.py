4ed8c3cd3fd16f7dabe2d6b49178f287baab5bb3
gaetanbrl <gaetan.bruel@jdev.fr>
2023-03-14 12:01:50 +0100

    add comment

--- srv/python/mviewerstudio_backend/app.py
+++ srv/python/mviewerstudio_backend/app.py
@@ -3,8 +3,8 @@ from .utils.register_utils import ConfigRegister
 
 app = create_app()
 
+# create register to ease application management
 app.register = ConfigRegister(app)
-
 app.register.create_register_from_file_system()
 
 if __name__ == "__main__":
