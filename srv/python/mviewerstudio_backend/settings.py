1301772e52ee9ffb60933d7b55d91b98a26b87fa
gaetanbrl <gaetan.bruel@jdev.fr>
2023-04-13 17:56:55 +0200

    use full path python config

--- srv/python/mviewerstudio_backend/settings.py
+++ srv/python/mviewerstudio_backend/settings.py
@@ -3,8 +3,8 @@ import os
 
 class Config:
     CONF_PATH_FROM_MVIEWER = os.getenv("CONF_PATH_FROM_MVIEWER", "apps/store")
-    EXPORT_CONF_FOLDER = os.getenv("EXPORT_CONF_FOLDER", "./store")
+    EXPORT_CONF_FOLDER = os.getenv("EXPORT_CONF_FOLDER", "/home/user/git/mviewer/apps/store")
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
     PROXY_WHITE_LIST = ["geobretagne.fr", "ows.region-bretagne.fr", "kartenn.region-bretagne.fr"]
-    MVIEWERSTUDIO_PUBLISH_PATH =  os.getenv("MVIEWERSTUDIO_PUBLISH_PATH", "public")
+    MVIEWERSTUDIO_PUBLISH_PATH =  os.getenv("MVIEWERSTUDIO_PUBLISH_PATH", "/home/user/git/mviewer/apps/public")
     DEFAULT_ORG = os.getenv("DEFAULT_ORG", "public")
