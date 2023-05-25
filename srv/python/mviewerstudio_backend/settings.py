55554f896e901c85b00fe55316e38e9b6bb592e9
gaetanbrl <gaetan.bruel@jdev.fr>
2023-05-25 10:28:46 +0200

    format settings file

--- srv/python/mviewerstudio_backend/settings.py
+++ srv/python/mviewerstudio_backend/settings.py
@@ -3,9 +3,19 @@ import os
 
 class Config:
     CONF_PATH_FROM_MVIEWER = os.getenv("CONF_PATH_FROM_MVIEWER", "apps/store")
-    CONF_PUBLISH_PATH_FROM_MVIEWER = os.getenv("CONF_PUBLISH_PATH_FROM_MVIEWER", "apps/public")
-    EXPORT_CONF_FOLDER = os.getenv("EXPORT_CONF_FOLDER", "/home/user/git/mviewer/apps/store")
+    CONF_PUBLISH_PATH_FROM_MVIEWER = os.getenv(
+        "CONF_PUBLISH_PATH_FROM_MVIEWER", "apps/public"
+    )
+    EXPORT_CONF_FOLDER = os.getenv(
+        "EXPORT_CONF_FOLDER", "/home/user/git/mviewer/apps/store"
+    )
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
-    PROXY_WHITE_LIST = ["geobretagne.fr", "ows.region-bretagne.fr", "kartenn.region-bretagne.fr"]
-    MVIEWERSTUDIO_PUBLISH_PATH =  os.getenv("MVIEWERSTUDIO_PUBLISH_PATH", "/home/user/git/mviewer/apps/public")
+    PROXY_WHITE_LIST = [
+        "geobretagne.fr",
+        "ows.region-bretagne.fr",
+        "kartenn.region-bretagne.fr",
+    ]
+    MVIEWERSTUDIO_PUBLISH_PATH = os.getenv(
+        "MVIEWERSTUDIO_PUBLISH_PATH", "/home/user/git/mviewer/apps/public"
+    )
     DEFAULT_ORG = os.getenv("DEFAULT_ORG", "public")
