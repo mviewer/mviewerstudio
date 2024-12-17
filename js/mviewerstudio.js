4fb1e0df29ebec4775efc178321d02d5d2c605f4
LoIc <loic.ecault@bretagne.bzh>
2024-12-17 14:15:36 +0100

    correction mapprint addlayers

--- js/mviewerstudio.js
+++ js/mviewerstudio.js
@@ -272,6 +272,8 @@ var newConfiguration = function (infos) {
     "opt-showhelp",
     "opt-coordinates",
     "opt-togglealllayersfromtheme",
+    "opt-mapprint",
+    "opt-addlayerstools",
     "SwitchCustomBackground",
     "SwitchAdvanced",
     "search-closeafterclick",
@@ -733,6 +735,8 @@ var getConfig = () => {
     'togglealllayersfromtheme="' +
       ($("#opt-togglealllayersfromtheme").prop("checked") === true) +
       '"',
+    'mapprint="' + ($("#opt-mapprint").prop("checked") === true) + '"',
+    'addlayerstools="' + ($("#opt-addlayerstools").prop("checked") === true) + '"',
   ];
 
   config.title = $("#opt-title").val();
