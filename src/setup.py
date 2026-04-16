86805e56a761bf19d9731c99cde5710491f97497
gaetanbrl <Vous@exemple.com>
2026-04-16 15:41:21 +0200

    add correct versions classifier

--- src/setup.py
+++ src/setup.py
@@ -34,6 +34,8 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Framework :: Flask",
         "Topic :: Internet :: WWW/HTTP",
